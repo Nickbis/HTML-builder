7d059724fb37ed1574cb63dbe70739571624df7e
Nickbis <nickbis3@gmail.com>
2022-11-06 22:03:12 +0300

    feat: task 4 completed

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,33 @@
+const fs = require('fs'); // Common JS
+const path = require('path');
 
+(async function copyDir() {
+    try {
+      await fs.promises.access(path.join(__dirname, 'files-copy'), fs.constants.F_OK);
+      await fs.promises.rm(
+        path.join(__dirname, 'files-copy'),
+        {recursive: true}
+      );
+      } catch (err) {
+      if (err) console.log('');
+      }
+  
+    await fs.promises.mkdir(
+      path.join(__dirname, 'files-copy')
+    );
+  
+    const files = await fs.promises.readdir(
+      path.join(__dirname, 'files')
+    );  
+  
+    for (let i = 0; i < files.length; i++) {
+      fs.copyFile(
+        path.join(__dirname, 'files', files[i]),
+        path.join(__dirname, 'files-copy', files[i]),
+        err => {
+          if (err) throw err;
+        }
+      ); 
+    }
+    return console.log('Сopying completed');
+  })();  
